b5b4543e292b9bf5344d14bc8c04804385aaadd0
之一 <nicolanazwj@gmail.com>
2023-10-23 01:01:19 +0800

    feat: 使用向量匹配知识库

--- src/api/index.js
+++ src/api/index.js
@@ -4,3 +4,6 @@ export function getPrompt(payload) {
     return request.post("/get_prompt", payload)
 }
 
+export function getSearch(payload) {
+    return request.post('/search', payload)
+}
--- src/pages/Knowledge.vue
+++ src/pages/Knowledge.vue
@@ -1,3 +1,35 @@
 <template>
-  <div>KNOWLEDGE</div>
+    <div>
+        <div class="flex items-center">
+            <el-input v-model="query" size="large" placeholder="请输入" clearable/>
+            <el-button :loading="loading" class="ml-4" type="primary" @click="onSubmit" size="large">搜索</el-button>
+        </div>
+
+        <div class="mt-4 h-[800px] rounded-4 p-4 max-h-1000px"
+             style="box-shadow: 0 0 0 1px var(--el-input-border-color,var(--el-border-color)) inset">
+            {{ content }}
+        </div>
+    </div>
 </template>
+
+<script setup>
+import {ref} from 'vue'
+import {getSearch} from "../api/index.js";
+import { marked } from 'marked';
+
+const query = ref('');
+const content = ref('');
+const loading = ref(false)
+
+const onSubmit = async () => {
+    loading.value = true
+    const res = await getSearch({query: query.value})
+    content.value = ''
+    console.log("Res =", res)
+    if (res.status === 200) {
+        console.log("res.data =", res.data);
+        content.value = res.data;
+    }
+    loading.value = false
+}
+</script>
