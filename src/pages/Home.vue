a7dee032965527abafe1c18fb9584b6926e18b5d
之一 <nicolanazwj@gmail.com>
2023-10-29 18:39:35 +0800

    feat: 更新聊天输入框样式

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,20 +3,23 @@
         <div class="chat-bot-wrap flex h-[80vh] rounded-[0.5rem] overflow-hidden w-full">
             <div class="chat-bot-history w-[336px] bg-[rgb(245,245,245)] px-[16px] py-[16px]">History is here</div>
             <div class="chat-bot-main h-full flex flex-col flex-1 p-[16px]">
-                <div class="chat-bot-modal flex-1">聊天窗口</div>
-                <div class="chat-bot-input flex-shrink-0 flex items-center">
-                    <el-input
-                        v-model="queryText"
-                        type="textarea"
-                        autosize
-                        placeholder="请输入对话内容，换行请使用Shift + Enter"
-                        class="input-with-select flex-1"
-                    >
-                        <template #append>
-                            <el-button :icon="Search" />
-                        </template>
-                    </el-input>
-                    <el-button :icon="Promotion" />
+                <div class="chat-bot-modal flex-1 relative">
+                    <div class="absolute top-0 left-0 w-full h-full overflow-y-scroll">
+                        <div>Real Content is here</div>
+                    </div>
+                </div>
+                <div class="chat-bot-input flex-shrink-0 bg-white pt-[16px]">
+                    <div class="chat-bot-input-wrap">
+                        <el-input
+                            v-model="queryText"
+                            type="textarea"
+                            :autosize="{ minRows: 1, maxRows: 4 }"
+                            placeholder="请输入对话内容，换行请使用Shift + Enter"
+                            class="input-with-select flex-1"
+                        >
+                        </el-input>
+                        <el-button :icon="Promotion" />
+                    </div>
                 </div>
             </div>
         </div>
@@ -59,3 +62,27 @@ const onGenerateClick = async () => {
     }
 };
 </script>
+
+<style lang="scss">
+.chat-bot-input {
+    .chat-bot-input-wrap {
+        background-color: rgb(245, 245, 245);
+        display: flex;
+        align-items: flex-end;
+        border-radius: 0.5rem;
+        .el-textarea__inner {
+            padding: 16px;
+            height: 100%;
+            background: transparent;
+            box-shadow: none;
+            resize: none;
+        }
+        .el-button {
+            background: none;
+            height: 53px;
+            font-size: 20px;
+            border: none;
+        }
+    }    
+}
+</style>
