7b6bb853d82651702360cb8f2a8d7393d2558aa0
之一 <nicolanazwj@gmail.com>
2023-10-29 20:20:39 +0800

    feat: 优化聊天样式

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,50 @@
 <template>
     <div class="chat-bot-container">
         <div class="chat-bot-wrap flex h-[80vh] rounded-[0.5rem] overflow-hidden w-full">
-            <div class="chat-bot-history w-[336px] bg-[rgb(245,245,245)] px-[16px] py-[16px]">History is here</div>
-            <div class="chat-bot-main h-full flex flex-col flex-1 p-[16px]">
+            <div class="chat-bot-history w-[336px] bg-[rgb(245,245,245)] px-[16px] py-[16px] rounded-lg">
+                History is here
+            </div>
+            <div class="chat-bot-main h-full flex flex-col flex-1 px-[16px] pt-4 pb-0">
                 <div class="chat-bot-modal flex-1 relative">
                     <div class="absolute top-0 left-0 w-full h-full overflow-y-scroll">
-                        <div>Real Content is here</div>
+                        <div>
+                            <div
+                                v-for="(message, index) in state.messages"
+                                :key="index"
+                                class="flex items-center p-4 rounded-lg"
+                                :class="{
+                                    'mt-4': index != 0,
+                                    'bg-[rgba(240,242,246,0.5)]': message.type === MessageType.User
+                                }"
+                            >
+                                <div
+                                    class="mr-4 w-8 h-8 bg-[rgb(255,108,108)] text-white rounded-[0.5rem] flex-shrink-0 flex items-center justify-center"
+                                    v-if="message.type === MessageType.User"
+                                >
+                                    <svg
+                                        viewBox="0 0 24 24"
+                                        aria-hidden="true"
+                                        focusable="false"
+                                        fill="currentColor"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        color="inherit"
+                                        class="eyeqlp51 st-emotion-cache-fblp2m ex0cdmw0"
+                                    >
+                                        <path fill="none" d="M0 0h24v24H0V0z"></path>
+                                        <path
+                                            d="M10.25 13a1.25 1.25 0 11-2.5 0 1.25 1.25 0 012.5 0zM15 11.75a1.25 1.25 0 100 2.5 1.25 1.25 0 000-2.5zm7 .25c0 5.52-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2s10 4.48 10 10zM10.66 4.12C12.06 6.44 14.6 8 17.5 8c.46 0 .91-.05 1.34-.12C17.44 5.56 14.9 4 12 4c-.46 0-.91.05-1.34.12zM4.42 9.47a8.046 8.046 0 003.66-4.44 8.046 8.046 0 00-3.66 4.44zM20 12c0-.78-.12-1.53-.33-2.24-.7.15-1.42.24-2.17.24a10 10 0 01-7.76-3.69A10.016 10.016 0 014 11.86c.01.04 0 .09 0 .14 0 4.41 3.59 8 8 8s8-3.59 8-8z"
+                                        ></path>
+                                    </svg>
+                                </div>
+                                <div
+                                    class="mr-4 w-8 h-8 text-white rounded-[0.5rem] flex-shrink-0 flex items-center justify-center overflow-hidden"
+                                    v-else
+                                >
+                                <img :src="UserImage" alt="user icon" />
+                                </div>
+                                <div class="flex-1">{{ message.content }}</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="chat-bot-input flex-shrink-0 bg-white pt-[16px]">
@@ -27,12 +66,54 @@
 </template>
 
 <script setup>
-import { computed, ref } from "vue";
+import { computed, ref, reactive } from "vue";
 import { getPrompt } from "../api";
 import { isEmpty, copyContent } from "../utils/index.js";
 import { ElMessage } from "element-plus";
 import { Promotion } from "@element-plus/icons-vue";
+import UserImage from '../assets/imgs/user.png';
 
+const MessageType = {
+    User: 1,
+    System: 2,
+};
+
+const state = reactive({
+    messages: [
+        {
+            type: MessageType.User,
+            content: `Hi`,
+        },
+        {
+            type: MessageType.System,
+            content: `Hello! How can I assist you today?`,
+        },
+        {
+            type: MessageType.User,
+            content: `Hi`,
+        },
+        {
+            type: MessageType.System,
+            content: `Hello! How can I assist you today?`,
+        },
+        {
+            type: MessageType.User,
+            content: `Hi`,
+        },
+        {
+            type: MessageType.System,
+            content: `Hello! How can I assist you today?`,
+        },
+        {
+            type: MessageType.User,
+            content: `Hi`,
+        },
+        {
+            type: MessageType.System,
+            content: `Hello! How can I assist you today?`,
+        },
+    ],
+});
 const queryText = ref();
 const outPutText = ref();
 const loading = ref(false);
@@ -83,6 +164,6 @@ const onGenerateClick = async () => {
             font-size: 20px;
             border: none;
         }
-    }    
+    }
 }
 </style>
